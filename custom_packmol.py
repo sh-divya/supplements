c5c887eca3d183d586fb35e57fba4550f64f540a
SarcasticMinion <divya.zs308@gmail.com>
2019-02-23 18:31:01 -0500

    Hopefully this works. Unsure about the parsing I did for the output file name from the Packmol custom string

--- custom_packmol.py
+++ custom_packmol.py
@@ -1,4 +1,20 @@
-from squid import files, structures
+from squid import files, structures, sysconst
 import os
 
-def 
+def custom_packmol_coords(packmol_custom_string):
+    
+    packmol_string_list = packmol_custom_string.split('_').split('\n')
+    packmol_output = [packmol_string_list[i+1] for i, word in enumerate(packmol_string_list) if words == 'output']
+
+    temp = open('temp.inp', 'a')
+    temp.write(packmol_custom_string)
+    temp.close()
+
+    temp_file_path = os.path.abspath('temp.inp')
+    os.system(packmol_path + ' < ' + temp_file_path)
+    packmol_coords = open(packmol_output).read()
+
+    os.system('rm temp.inp')
+    os.system(('rm ' + packmol_output))
+
+    return packmol_coords
