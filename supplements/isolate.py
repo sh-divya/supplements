a492cd0349ac6d3667c954cd7af9eb8b9eebeace
Divya Sharma <divya.zs308@gmail.com>
2019-03-22 14:01:14 -0400

    Isolate generator in orginal isolate python file

--- supplements/isolate.py
+++ supplements/isolate.py
@@ -25,3 +25,27 @@ def isolate_atom_types(types, dump_obj, mol_len):
 			pass
 
 	return frames
+
+def isolate_gen(types, dump_obj, mol_len):
+	
+	count = 0
+	atoms_list = []
+
+	for line in dump_obj:
+		words = line.split()
+
+		if count == mol_len:
+			yield atom_list
+			count = 0
+			atoms_list = []
+
+		try:
+			if int(words[0]) in types:
+				atom_type = types[int(words[0])]
+				x = float(words[1])
+				y = float(words[2])
+				z = float(words[3])
+				count = count + 1
+				atoms_list.append(structures.Atom(atom_type, x, y, z, index = count))
+		except ValueError:
+			pass
