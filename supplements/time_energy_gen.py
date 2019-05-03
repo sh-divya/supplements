e08008fc720fdb4586c9eacde1db91a95366188e
Divya Sharma <divya.zs308@gmail.com>
2019-05-03 18:17:02 -0400

    Final changes to time_energy_gen. Works fine.

--- supplements/time_energy_gen.py
+++ supplements/time_energy_gen.py
@@ -7,8 +7,25 @@ def read_log_or_out(fileObj, thermo_output_list):
             if data[0] == 'Loop':
                 flag = False
             if flag:
-                yield (int(data[0]), float(data[1]))
+                yield data
+                #yield (int(data[0]), float(data[1]))
             if data == thermo_output_list:
                 flag = True
         except (IndexError, ValueError):
             pass
+
+def read_log_or_out_newer(fileObj, thermo_output_list):
+
+    flag = True
+    for line in fileObj:
+        data = line.split()
+        try:
+            if data[0] == 'Loop':
+                flag = True
+            if flag:
+                yield data
+                #yield (int(data[0]), float(data[1]))
+            if data == thermo_output_list:
+                flag = False
+        except (IndexError, ValueError):
+            yield data
