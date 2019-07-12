12c6385d6fd3d88861485fa0e183e4e4f33d0a55
Divya <ds2299@cornell.edu>
2019-07-12 14:23:06 -0400

    Added function to get opls dihedral energy values

---
+++ supplements/opls_energies.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def dihedral_energy(*coeffs, theta = [i for i in range(0, 370, 10)]):
+    
+    energy_values = []
+    for angle in theta:
+        energy_point = coeffs[0]*(1+np.cos(np.radians(angle))) + coeffs[1]*(1-np.cos(np.radians(2*angle))) +  coeffs[2]*(1+np.cos(np.radians(angle)))
+        energy_values.append(energy_point)
+
+    return (energy_values, theta)
+
