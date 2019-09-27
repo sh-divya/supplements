9851149f5aeb96ec60090e2710dffa06fef1dd6d
Divya <ds2299@cornell.edu>
2019-09-27 18:45:06 -0400

    Modified the rg function. Molecules must be loaded with the labels

--- supplements/com_rg.py
+++ supplements/com_rg.py
@@ -1,35 +1,16 @@
 import math
-from squid import structures
+from squid import structures, units
 
-def get_com(molecule, types):
-    
-    com_x = 0
-    com_y = 0
-    com_z = 0
-    mol_wt = 0
-    for atom in molecule:
-        m_i = types[atom.element]
-        mol_wt += m_i
-        (x_i, y_i, z_i) = atom.flatten()
-        com_x += m_i*x_i
-        com_y += m_i*y_i
-        com_z += m_i*z_i
 
-    com_x = com_x/mol_wt
-    com_y = com_y/mol_wt
-    com_z = com_z/mol_wt
-
-    return(com_x, com_y, com_z)
-    
 def get_rg(mol, types):
 
-    (c_x, c_y, c_z) = get_com(mol, types)
+    (c_x, c_y, c_z) = mol.get_center_of_mass()
     rx_2 = 0
     ry_2 = 0
     rz_2 = 0
     mol_wt = 0
-    for atom in mol:
-        m_i = types[atom.element]
+    for atom in mol.atoms:
+        m_i = units.elem_weight(atom.element)
         mol_wt += m_i 
         (x_i, y_i, z_i) = atom.flatten()
         rx_2 += m_i*((x_i-c_x)**2)
