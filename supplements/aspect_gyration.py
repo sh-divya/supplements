06f8142e88a2a7f74ad842b1a07310c020cb176c
Divya Sharma <divya.zs308@gmail.com>
2020-04-24 13:29:39 -0400

    Added index check in aspect_gyration

--- supplements/aspect_gyration.py
+++ supplements/aspect_gyration.py
@@ -5,20 +5,23 @@ from squid import geometry
 from squid.utils import units
 
 
-def get_rg(mol):
+def get_rg(mol, indices=None):
 
     (c_x, c_y, c_z) = mol.get_center_of_mass()
     rx_2 = 0
     ry_2 = 0
     rz_2 = 0
     mol_wt = 0
-    for atom in mol.atoms:
-        m_i = units.elem_weight(atom.element)
-        mol_wt += m_i 
-        (x_i, y_i, z_i) = atom.flatten()
-        rx_2 += m_i*((x_i-c_x)**2)
-        ry_2 += m_i*((y_i-c_y)**2)
-        rz_2 += m_i*((z_i-c_z)**2)
+    if not indices:
+        indices = list(range(1, len(mol.atoms) + 1))
+    for a, atom in enumerate(mol.atoms):
+        if a + 1 in indices:
+            m_i = units.elem_weight(atom.element)
+            mol_wt += m_i 
+            (x_i, y_i, z_i) = atom.flatten()
+            rx_2 += m_i*((x_i-c_x)**2)
+            ry_2 += m_i*((y_i-c_y)**2)
+            rz_2 += m_i*((z_i-c_z)**2)
         
     rg = math.sqrt((rx_2 + ry_2 + rz_2)/mol_wt)
 
